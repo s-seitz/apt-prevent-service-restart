3d609c0c0d12038ae8019f8c814f627ec674240f
Stephan Seitz <s.seitz@bnotk.de>
2019-03-12 12:03:26 +0100

    smaller cleanups and shellcheck-conformity

--- usr/sbin/policy-rc.d
+++ usr/sbin/policy-rc.d
@@ -1,21 +1,37 @@
 #!/bin/bash -e
-# Hook File
+#
+# /usr/sbin/policy-rc.d
+#
+# This script is hooked by apt/dpkg service stop/start/restart-actions
+# Depending on the RC, apt/dpkg prevents touching the state of a service.
+# Refer to invoke-rc LSB Returncodes (README-invoke-rc.d-RC.md)
+#
+RC_OK=0
+RC_SCRIPT_UNKNOWN=100
+RC_ACTION_NOT_ALLOWED=101
 
-test -f /etc/default/policy-rc && . /etc/default/policy-rc
+# shellcheck source=/dev/null
+test -f "/etc/default/policy-rc" && source "/etc/default/policy-rc"
+
+# disabled by default.
 DISABLE_RESTART_DURING_UPDATE=${DISABLE_RESTART_DURING_UPDATE:-0}
-POLICY_RC_TMPFILE=${POLICY_RC_TMPFILE:-/tmp/policy-rc.tmp}
+POLICY_RC_TMPFILE="${POLICY_RC_TMPFILE:-/tmp/policy-rc.tmp}"
+SYSLOG_PRIORITY="${SYSLOG_PRIORITY:-local0.info}"
 
-SERVICE_NAME=$1
-SERVICE_ACTION=$2
+SERVICE_NAME="$1"
+SERVICE_ACTION="$2"
 
-if [ $[DISABLE_RESTART_DURING_UPDATE] -eq 1 ]
+if [ -z "${SERVICE_NAME}" ]
 then
-	msg="Prevented Service ${SERVICE_NAME} from Action ${SERVICE_ACTION}"
-	echo "$(date +"%b %e %X") $msg" >> ${POLICY_RC_TMPFILE}
-	echo $msg | logger
-	RC=101
+  RC=$RC_SCRIPT_UNKNOWN
+elif [ $((DISABLE_RESTART_DURING_UPDATE)) -eq 1 ]
+then
+  msg="Prevented Service ${SERVICE_NAME} from Action ${SERVICE_ACTION}"
+  echo "$(date +"%b %e %X") $msg" >> "${POLICY_RC_TMPFILE}"
+  logger --priority "${SYSLOG_PRIORITY}" "$msg"
+  RC=$RC_ACTION_NOT_ALLOWED
 else
-	RC=0
+	RC=$RC_OK
 fi
 exit $RC
 
